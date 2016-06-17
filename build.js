507776b0595fc6c7e5ae0cde0f3cb905e75732e2
Mark van Duijker <mark@duyker.nl>
2016-06-17 08:52:55 +0200

    Implemented incremental sync and easier access to data through lowdb

--- build.js
+++ build.js
@@ -3,21 +3,25 @@ require('dotenv').config({silent: true});
 console.log('Building...');
 
 let App = require('./lib/app'),
-  db = require('./public/db/entries.json'),
+  db = require('lowdb')('public/db/db-test.json', { writeOnChange: false }),
   app = new App({
     env: process.env.NODE_ENV,
     baseUrl: process.env.BASE_URL,
     gaTrackingCode: process.env.GA_TRACKING_CODE
   });
 
-db.forEach((entry) => {
-  if (entry.sys.contentType.sys.id == 'homepage') {
-    app.render('/', () => {
-      entry.fields.template = 'home';
+// render homepage
+app.render('/', () => {
+  const entry = db.get('entries').find({sys : { contentType : { sys: {id : 'homepage'}}}}).value();
 
-      return entry.fields;
-    });
-  } else if (entry.sys.contentType.sys.id == 'page' && entry.fields.path) {
+  entry.fields.template = 'home';
+
+  return entry.fields;
+});
+
+// render pages
+db.get('entries').filter({sys : { contentType : { sys: {id : 'page'}}}}).value().forEach((entry) => {
+  if (entry.fields.path) {
     let path = '/' + entry.fields.path['nl-NL'] + '/';
     app.render(path, () => {
       entry.fields.template = 'content';
--- sync-contentful.js
+++ sync-contentful.js
@@ -1,23 +1,71 @@
 require('dotenv').config({silent: true});
 
 let contentful = require('contentful'),
-  fs = require('fs-extra');
+  _ = require('lodash'),
+  db = require('lowdb')('public/db/db-test.json', { writeOnChange: false }),
+  alwaysFullSync = process.env.CONTENTFUL_FULL_SYNC || false;
 
 let client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE,
   accessToken: process.env.CONTENTFUL_API_KEY
 });
 
-client.sync({initial: true})
-  .then((response) => {
-    fs.outputJson('public/db/entries.json', response.entries, (err) => {
-      if (err) {
-        throw err;
-      }
+let updateItem = (type, item) => {
+  db.get(type).find({"sys" : {"id": item.sys.id}}).assign(item).value();
+};
+
+let deleteItem = (type, item) => {
+  db.get(type).remove({"sys": {"id": item.sys.id}}).value();
+};
+
+const incrementalSync = (syncToken) => {
+  client
+    .sync({nextSyncToken: syncToken})
+    .then((response) => {
+      // update entries
+      _(response.entries).forEach((entry) => {
+        updateItem('entries', entry);
+      });
+
+      // update assets
+      _(response.entries).forEach((asset) => {
+        updateItem('assets', asset);
+      });
+
+      // deleted entries
+      _(response.deletedEntries).forEach((entry) => {
+        deleteItem('entries', entry);
+      });
+
+      // deleted assets
+      _(response.deletedAssets).forEach((asset) => {
+        deleteItem('assets', asset);
+      });
+
+      // write sync token
+      db.set('sync-token', response.nextSyncToken).value();
+
+      db.write();
+    })
+};
+
+const fullSync = () => {
+  client.sync({initial: true})
+    .then((response) => {
+      db.set('entries', response.entries).value();
+      db.set('assets', response.assets).value();
+      db.set('sync-token', response.nextSyncToken).value();
+
+      db.write();
     });
-    fs.outputJson('public/db/assets.json', response.assets, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  });
+};
+
+if (alwaysFullSync || !db.has('sync-token').value()) {
+  console.log('Syncing everything!');
+  fullSync();
+} else {
+  console.log('Incremental sync!');
+  incrementalSync(
+    db.get('sync-token').value()
+  );
+}

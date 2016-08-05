a52f808374a443abe72bdea0121fbf76b74d02e8
Mark van Duijker <mark@duyker.nl>
2016-08-05 22:43:20 +0200

    Added some more tests and removed lowdb from building pages

---
+++ test/lib/build/retrieve-content-pages.js
@@ -0,0 +1,34 @@
+let test = require('tape'),
+  retrieveContentPages = require('../../../lib/build/retrieve-content-pages');
+
+test('retrieves content pages from database and creates page objects', assert => {
+  const e = {};
+
+  e.db = require('./database.json');
+
+  retrieveContentPages(e);
+
+  assert.same(
+    {
+      title: "Page 1",
+      path: "page-1",
+      template: "content"
+    },
+    e.pages['/page-1/'],
+    'Has page 1 with route, transformed fields and template content'
+  );
+
+  assert.same(
+    {
+      title: "Page 2",
+      path: "page-2",
+      template: "content"
+    },
+    e.pages['/page-2/'],
+    'Has page 2 with route, transformed fields and template content'
+  );
+
+  assert.equal(2, Object.keys(e.pages).length, 'Retrieved exactly 2 pages');
+
+  assert.end();
+});
---
+++ test/lib/build/retrieve-homepage.js
@@ -0,0 +1,21 @@
+let test = require('tape'),
+  retrieveHomepage = require('../../../lib/build/retrieve-home-page');
+
+test('retrieves homepage from database and creates page object', assert => {
+  const e = {};
+
+  e.db = require('./database.json');
+
+  retrieveHomepage(e);
+
+  assert.same(
+    {
+      title: "Example homepage title",
+      template: "home"
+    },
+    e.pages['/'],
+    'Has page with route / with transformed fields and template home'
+  );
+
+  assert.end();
+});

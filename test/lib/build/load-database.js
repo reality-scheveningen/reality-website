a8a2be4fffa2a71df305f2ff6109bcaf539a1514
Mark van Duijker <mark@duyker.nl>
2016-08-07 20:56:50 +0200

    Tests for loading database

---
+++ test/lib/build/load-database.js
@@ -0,0 +1,16 @@
+let test = require('tape'),
+  loadDatabase = require('../../../lib/build/load-database');
+
+test('loads database', assert => {
+  const e = {
+    config: {
+      databasePath: `${__dirname}/database.json`
+    }
+  };
+
+  loadDatabase(e);
+
+  assert.equal(true, e.db.entries instanceof Array, 'Database is loaded');
+
+  assert.end();
+});

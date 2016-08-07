e9a81cd4e3dffdfdf00ce30e3637ed7dca2cc6e9
Mark van Duijker <mark@duyker.nl>
2016-08-07 22:27:20 +0200

    Added test for render html

---
+++ test/lib/build/render/render-html.js
@@ -0,0 +1,39 @@
+let test = require('tape'),
+  proxyquire = require('proxyquire').noCallThru();
+
+test('renders the html templates', assert => {
+
+  let e = {
+    page: {
+      sitePath: 'site/path',
+      template: 'some-template'
+    }
+  };
+
+  let called = false;
+  const renderHtml = proxyquire('../../../../lib/build/render/render-html', {
+    "pug": {
+      renderFile: (path, data, cb) => {
+        assert.equal('site/path/templates/some-template.pug', path);
+        assert.same(e.page, data);
+
+        called = true;
+
+        cb();
+      }
+    }
+  });
+
+  const result = renderHtml(e);
+
+  result.then(res => {
+    assert.equal(true, called, 'Pug render file has been called');
+    assert.equal(true, res.hasOwnProperty('html'), 'Html has been set');
+
+    assert.end();
+  }).catch(err => {
+    assert.fail('Promise failed with: ' + err);
+
+    assert.end();
+  });
+});

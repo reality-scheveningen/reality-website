25564ea0ff83c24ba982561e78d4e35d54b2d34a
Mark van Duijker <mark@duyker.nl>
2016-08-17 13:00:52 +0200

    Refactor determine sync strategy

---
+++ test/lib/sync/determine-sync-strategy.js
@@ -0,0 +1,14 @@
+let test = require('tape')
+let determineSyncStrategy = require('../../../lib/sync/determine-sync-strategy')
+
+test('ensure full sync when no sync token', assert => {
+  const e = {
+    config: {},
+    db: {}
+  }
+
+  const result = determineSyncStrategy(e)
+
+  assert.equal(true, result.config.fullSync)
+  assert.end()
+})

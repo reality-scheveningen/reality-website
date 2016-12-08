00f854bf9f30fcad9fd624659b51ac7ab7990ee6
Mark van Duijker <mark@duyker.nl>
2016-12-08 20:04:03 +0100

    Redirect to canonical not needed anymore

--- site/static/assets/js/redirect-to-canonical-origin.js
+++
@@ -1,8 +0,0 @@
-function redirectToCanonicalOrigin (baseUrl, window) { // eslint-disable-line
-  // http://tosbourn.com/a-fix-for-window-location-origin-in-internet-explorer/
-  var origin = window.location.origin || window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')
-
-  if (origin !== baseUrl) {
-    window.location = baseUrl + window.location.pathname + window.location.hash
-  }
-}

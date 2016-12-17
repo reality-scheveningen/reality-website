25a5af0df10680c6ba4fa590a6557c727d9452c5
Mark van Duijker <mark@duyker.nl>
2016-12-17 14:04:23 +0100

    Added console teaser, because we can.

---
+++ site/static/assets/js/console-teaser.js
@@ -0,0 +1,13 @@
+if (console && console.log) {
+  var style = 'font: 1.3rem "Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif; color: #333'
+
+  console.log(
+    '%c🎉 Howdy! Feel free to peek around! 🎉',
+    'font: 1.3rem "Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif; color: #333'
+  )
+
+  console.log(
+    '%cEven better check out the source code at https://github.com/reality-scheveningen/reality-website',
+    'font: 1.1rem "Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif; color: #333'
+  )
+}
